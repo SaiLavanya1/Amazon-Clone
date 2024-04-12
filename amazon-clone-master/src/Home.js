c48faf1d184cf9d2101f30aa293a04c25fef7798
Sailavanya Narthu <98532973+SaiLavanya1@users.noreply.github.com>
2024-04-11 20:54:21 -0700

    Update Home.js

--- amazon-clone-master/src/Home.js
+++ amazon-clone-master/src/Home.js
@@ -1,72 +1,70 @@
-import { Drawer } from "@material-ui/core";
-import React, { useState } from "react";
-import Checkout from "./Checkout";
+import React from "react";
 import "./Home.css";
-import { sizing } from "@material-ui/core";
 import Product from "./Product";
 
-const Home = () => {
-  const [drawer, setDrawer] = useState(false);
-
+function Home() {
   return (
     <div className="home">
       <div className="home__container">
         <img
-          src="https://images-eu.ssl-images-amazon.com/images/G/31/AmazonVideo/2020/X-site/Multititle/Aug/1500x600_Hero-Tall_np._CB404803728_.jpg"
           className="home__image"
+          src="https://tecnovery.com/wp-content/uploads/2020/03/1366_2000-3.jpg"
+          alt=""
         />
-        <div className="home__row" style={{ marginTop: "300px" }}>
+
+        <div className="home__row">
           <Product
-            id="12849"
-            title="The Most helpful book in the world, this book gives you basic knowledge about life (ofcourse if you do not know)."
-            price={29.99}
-            image={`https://m.media-amazon.com/images/I/410b3ttpfAL.__AC_SY200_.jpg`}
+            id="12321341"
+            title="The Lean Startup: How Constant Innovation Creates Radically Successful Businesses Paperback"
+            price={11.96}
             rating={5}
+            image="https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._SX325_BO1,204,203,200_.jpg"
           />
           <Product
-            id="3534345"
-            title="The Most helpful book in the world, this book gives you basic knowledge about life (ofcourse if you do not know)."
-            price={29.99}
-            image={`https://m.media-amazon.com/images/I/410b3ttpfAL.__AC_SY200_.jpg`}
-            rating={5}
+            id="49538094"
+            title="Fujifilm Instax Mini 11 Instant Camera With Case, 60 Fujifilm Films"
+            price={139.99}
+            rating={4}
+            image="https://i.ebayimg.com/images/g/HPgAAOSwAQdjfoUc/s-l400.jpg"
           />
         </div>
 
         <div className="home__row">
           <Product
-            id="24242"
-            title="The Most helpful book in the world, this book gives you basic knowledge about life (ofcourse if you do not know)."
-            price={29.99}
-            image={`https://m.media-amazon.com/images/I/410b3ttpfAL.__AC_SY200_.jpg`}
-            rating={5}
+            id="4903850"
+            title="BURBERRY BE 2255Q 3657 Havana Bordeaux Plastic Square Eyeglasses 51mm"
+            price={121.99}
+            rating={3}
+            image="https://m.media-amazon.com/images/I/51P8rg+6ABL._AC_SY695_.jpg"
           />
           <Product
-            id="23553647"
-            title="The Most helpful book in the world, this book gives you basic knowledge about life (ofcourse if you do not know)."
-            price={29.99}
-            image={`https://m.media-amazon.com/images/I/410b3ttpfAL.__AC_SY200_.jpg`}
-            rating={5}
+            id="23445930"
+            title="Apple Pencil 2nd Generation with Magnetic Wireless Charging"
+            price={28.99}
+            rating={4}
+            image="https://m.media-amazon.com/images/I/41yDnYrNctL._AC_SX679_.jpg"
           />
           <Product
-            id="359473"
-            title="The Most helpful book in the world, this book gives you basic knowledge about life (ofcourse if you do not know)."
-            price={29.99}
-            image={`https://m.media-amazon.com/images/I/410b3ttpfAL.__AC_SY200_.jpg`}
-            rating={5}
+            id="3254354345"
+            title="KIBAGA Beautiful Greeting Cards Set of 60 with Envelopes & Stickers"
+            price={7.99}
+            rating={4}
+            image="https://m.media-amazon.com/images/I/71m2kZBUDpL._AC_SL1500_.jpg"
           />
         </div>
+
         <div className="home__row">
           <Product
-            id="4839274"
-            title="The Most helpful book in the world, this book gives you basic knowledge about life (ofcourse if you do not know)."
-            price={29.99}
-            image={`https://m.media-amazon.com/images/I/410b3ttpfAL.__AC_SY200_.jpg`}
+            id="90829332"
+            title="Samsung Galaxy Tab S9 FE+ 12.4” 256GB Android Tablet, IP68 Water- and Dust-Resistant, Long Battery Life, Powerful Processor, S Pen, 8MP Camera, Lightweight"
+            price={569.99}
             rating={5}
+            image="https://m.media-amazon.com/images/I/51M3Xt+8OiL._AC_SL1280_.jpg"
           />
         </div>
       </div>
     </div>
   );
-};
+}
 
 export default Home;
